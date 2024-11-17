fd89c41fa1b376f203a13362c0a23222139cec89
Alexandra Bunch <121991056+oldfashionedjelly@users.noreply.github.com>
2024-11-17 10:12:40 -0500

    Setting up pins before I screw something up

---
+++ robot.ino
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+#define PIN_PB_START           0
+#define PIN_MTR1_ENCB          1
+#define PIN_MTR1_ENCA          2
+#define PIN_MTR2_ENCB          3
+#define PIN_MTR2_ENCA          4
+#define PIN_MTR1_DIR_FWD       5
+#define PIN_MTR1_DIR_REV       6
+#define PIN_MTR2_DIR_FWD       7
+#define PIN_MTR2_DIR_REV       8
+#define PIN_MTR1_PWM           9
+#define PIN_MTR2_PWM          10
+#define PIN_SONIC_PULSE       11
+#define PIN_SONIC_TRIGGER     12
+
+//I think these are right but if something is wrong then that's probably why
+#define ENCODER_COUNTS_PER_REV  540     
+#define MM_PER_REV              235.62     
+#define ENCODER_COUNTS_90_DEG   135   
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
